ac93927c2c264cb85ab523eb9f0fde2dd0bc6510
dcarrionp <dcportilla2003@gmail.com>
2024-05-23 08:17:54 -0500

    Agregar componentes

--- eva64carriondiego/src/app/app.routes.ts
+++ eva64carriondiego/src/app/app.routes.ts
@@ -1,3 +1,12 @@
 import { Routes } from '@angular/router';
+import { CrearComponent } from './components/crear/crear.component';
+import { LeerComponent } from './components/leer/leer.component';
+import { EliminarComponent } from './components/eliminar/eliminar.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+
+    {path:"crear", component: CrearComponent},
+    {path:"leer", component: LeerComponent},
+    {path:"eliminar", component: EliminarComponent},
+
+];
---
+++ eva64carriondiego/src/app/components/leer/leer.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-leer',
+  standalone: true,
+  imports: [],
+  templateUrl: './leer.component.html',
+  styleUrl: './leer.component.css'
+})
+export class LeerComponent {
+
+}
